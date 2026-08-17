bf4345020a711d450d9601df57f31c371f98e6a4
Lukasz <lukasz92626@gmail.com>
2026-08-18 00:05:33 +0200

    feat: add repository interfaces and unit of work contract.

---
+++ Library.Domain/Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.Domain.Interfaces;
+
+public interface IBookRepository
+{
+    Task<Book?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Book>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<IQueryable<Book>> GetQueryable();
+    Task AddAsync(Book book, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Book book, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<bool> IsbnExistsAsync(string isbn, CancellationToken cancellationToken = default);
+}
---
+++ Library.Domain/Interfaces/IRentalRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.Domain.Interfaces;
+
+public interface IRentalRepository
+{
+    Task<Rental?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Rental>> GetActiveRentalsByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Rental>> GetRentalsByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Rental>> GetOverdueRentalsAsync(CancellationToken cancellationToken = default);
+    Task AddAsync(Rental rental, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Rental rental, CancellationToken cancellationToken = default);
+}
